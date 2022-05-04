0daeea8ae2a5f96bfae9c09283779a77a5f2fa6d
Miguel Bogota <miguelbogota.rico@outlook.com>
2022-05-04 10:31:07 -0500

    Fixes export

--- public/types/index.ts
+++ public/types/index.ts
@@ -1,3 +1,3 @@
-export type { APIResponse } from './api-response';
+export type { APIWorkError, APIWorkResponse, APIWorkResponseList } from './api-response';
 export type { Work } from './work';
 export type { ContentV1 } from './content-v1';
