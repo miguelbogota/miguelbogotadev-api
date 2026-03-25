6b4ccfb3d7d19d9e3fa5d8bf0db0de0e46f11028
Miguel Bogota <miguelbogota.rico@outlook.com>
2026-03-25 12:21:10 -0500

    Change the struture of the project
    Changes the old server for a more minimalistic approach where only the data is store and then use in the app needed.

--- public/types/index.ts
+++
@@ -1,3 +0,0 @@
-export type { APIWorkError, APIWorkResponse, APIWorkResponseList } from './api-response';
-export type { Work } from './work';
-export type { ContentV1 } from './content-v1';
--- src/_content/index.ts
+++
@@ -1,8 +0,0 @@
-import { contentV1 } from './content-v1';
-
-/**
- * List with all the content values.
- */
-const content = [contentV1];
-
-export default content;
